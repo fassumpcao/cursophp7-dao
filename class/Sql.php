0aa80beee0bab41475c663131d0ddf55fe3c7fba
fassumpcao <felipe.p.assumpcao@gmail.com>
2019-05-29 22:15:48 +0000

    Criado a classe Usuario.

--- class/Sql.php
+++ class/Sql.php
@@ -9,17 +9,17 @@
 
 		public function __construct(){
 
-			$this->conn = new PDO("mysql:host=localhost;dbname=teste_desenvolvimento", "root", "");
+			$this->conn = new PDO("mysql:host=localhost;dbname=teste_desenvolvimento2", "root", "");
 		}
 
-		private function setParams($statment, $parameters = array()){
+		private function setParams($statement, $parameters = array()){
 			foreach ($parameters as $key => $value) {
-				$this->setParam($key, $value);
+				$this->setParam($statement, $key, $value);
 			}
 		}
 
-		private function setParam($statment, $key, $value){
-			$statment->bindParam($key, $value);
+		private function setParam($statement, $key, $value){
+			$statement->bindParam($key, $value);
 		}
 
 		public function query($rawQuery, $params = array()){
--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require_once("config.php");
 
+/*
 $sql = new Sql();
 
 $teste = $sql->select("SELECT * FROM adms_usuarios");
 
 echo json_encode($teste);
 
+*/
+
+$userObj = new Usuario();
+
+$userObj->loadById(1);
+
+echo $userObj;
  ?>
